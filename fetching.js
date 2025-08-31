3e3f95cad379a0f64291072af9c285e6288c3a45
Felipe Moya <fmoyagallo@gmail.com>
2025-08-31 15:27:26 -0400

    clase 8 revisada

--- fetching.js
+++ fetching.js
@@ -2,28 +2,28 @@
 
 // ASINCRONISMO
 
-// ---> Una pericion HTTP --> PROMESA
+// ---> Una peticion HTTP --> JS devuelve una PROMESA, la cual debe trabajarse (desencriptarse)
 
 // 2 FORMAS
-// FORMA 1: try>catch + async/await
+// FORMA 1: try/catch + async/await
 
-// REVISAR ESTO, quizas falta el catch en el try
 const obtenerPosteos = async () => {
   let contenedor = document.getElementById("container");
   //   console.log(contenedor);
   try {
-    // disparar una peticion HTTP. Primer parámetro el ENDPOINT, segundo el OBJETO DE CONFIGURACION
+    // fetch: disparar una peticion HTTP. Primer parámetro el ENDPOINT, segundo el OBJETO DE CONFIGURACION
     // OBJETO CONFIGURACION incluye methon, body, headers, etc
     // method por defecto del fetch es GET
-    const posteos = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    const posteos = await fetch("https://jsonplaceholder.typicode.com/postss", {
       method: "GET",
-      // body: // lugar en donde yo mando info del front al back,
+      // body: , // lugar en donde yo mando info del front al back, cuando uno CREA o actualiza algo
       // headers: {
       //     token,
       //     tipo dato
+      // }
     });
 
-    // console.log(posteos); // esto arrojará una "promesa" si no tengo async y await
+    // console.log(posteos); // async/await transforman la "promesa" en una respuesta
 
     // En local storage usamos JSON.parse(variable), pero en Fetch usamos variable.json()
     // Para Post, seguiremos usando JSON.stringify
@@ -36,8 +36,12 @@ const obtenerPosteos = async () => {
     });
 
     contenedor.innerHTML = htmlPosteos;
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+    contenedor.innerHTML = "<div><h2>Algo salio mal</h2></div>";
+  }
 };
+// obtenerPosteos();
 
 // FORMA 2: PROMESAS, cambiar await por then
 // fetch devuelve una promesa, por eso se aplica un then
@@ -45,6 +49,7 @@ const obtenerPosteos = async () => {
 const obtenerPosteos2 = () => {
   let contenedor = document.getElementById("container");
   const posteos = fetch("https://jsonplaceholder.typicode.com/posts");
+  // en el callback del .then SIEMPRE llega una respuesta, y en el del .catch SIEMPRE llega un error
   posteos
     .then((respuesta) => respuesta.json())
     .then((posteosListos) => {
@@ -55,27 +60,29 @@ const obtenerPosteos2 = () => {
       contenedor.innerHTML = htmlPosteos;
     })
     .catch((error) => {
-      console.log("Algo salio mal");
+      console.log(error);
+      contenedor.innerHTML = "<div><h2>Algo salio mal</h2></div>";
     });
 };
 
 obtenerPosteos2();
 
 // IMPORTANTE, SIEMPRE mejor usar async y await, aunque se haga un POST, porque
-// SIEMPRE querremos evaluar la respuesta
+// SIEMPRE vamos a querer evaluar la respuesta
 const crearPosteo = async () => {
-  // harcodeado, simulacion estática de algo dinámico
   try {
+    // harcodeado, simulacion estática de algo dinámico
     let nuevoPosteo = {
       title: "pepe",
       body: "este es el mejor post",
       userId: 1,
     };
 
-    // EN GET Y POST en general no va ID, no así en DELETE, PATCH, PUT
+    // EN GET Y POST en general no va ID en el ENDPOINT, no así en DELETE, PATCH, PUT
     // Si usaremos DELETE, normalmente el ENDPOINT tendrá el ID, y NO sería necesario un body
     const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
       method: "POST",
+      // RECORDAR, SIEMPRE que enviamos info del front al back, string JSON
       body: JSON.stringify(nuevoPosteo),
     });
     const responseParseada = await response.json();
@@ -97,8 +104,10 @@ const actualizarPosteo = async () => {
       title: "juan",
     };
 
-    // EN GET Y POST en general no va ID, no así en DELETE, PATCH, PUT
-    // Si usaremos DELETE, normalmente el ENDPOINT tendrá el ID, y NO sería necesario un body
+    // Para ACTUALIZAR, SI será necesario indicar el id en el ENDPOINT
+    // PUT agarra TODO el objeto viejo y lo reemplaza por el nuevo, es un peligro, porque se pueden
+    // perder atributos que no queríamos modificar
+    // Para solo modificar 1 o varios atributos, usar PATCH
     const response = await fetch(
       "https://jsonplaceholder.typicode.com/posts/3",
       {
